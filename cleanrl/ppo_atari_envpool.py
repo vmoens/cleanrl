b7da90fa15eaef1aa4c7b93989dcc84a6b5a6868
Vincent Moens <vmoens@meta.com>
2024-09-04 17:45:40 +0100

    amend

--- cleanrl/ppo_atari_envpool.py
+++ cleanrl/ppo_atari_envpool.py
@@ -357,7 +357,7 @@ if __name__ == "__main__":
         writer.add_scalar("losses/entropy", entropy_loss.item(), global_step)
         writer.add_scalar("losses/old_approx_kl", old_approx_kl.item(), global_step)
         writer.add_scalar("losses/approx_kl", approx_kl.item(), global_step)
-        writer.add_scalar("losses/clipfrac", np.mean(clipfracs), global_step)
+        writer.add_scalar("losses/clipfrac", torch.stack(clipfracs).mean(), global_step)
         writer.add_scalar("losses/explained_variance", explained_var, global_step)
         print("SPS:", int(global_step / (time.time() - start_time)))
         writer.add_scalar("charts/SPS", int(global_step / (time.time() - start_time)), global_step)
