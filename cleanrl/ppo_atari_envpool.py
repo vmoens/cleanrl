1b62aae874a04e981ff43ad139eca1e4175f0f38
Vincent Moens <vmoens@meta.com>
2024-09-05 08:16:58 +0100

    amend

--- cleanrl/ppo_atari_envpool.py
+++ cleanrl/ppo_atari_envpool.py
@@ -397,28 +397,30 @@ if __name__ == "__main__":
                 else:
                     g.replay()
 
-                clipfracs += [clipfrac.clone()]
+                # TODO
+                # clipfracs += [clipfrac.clone()]
 
             # TODO
             # if args.target_kl is not None and approx_kl > args.target_kl:
             #     break
 
         # TODO
-        y_pred, y_true = b_values.cpu().numpy(), b_returns.cpu().numpy()
-        var_y = np.var(y_true)
-        explained_var = np.nan if var_y == 0 else 1 - np.var(y_true - y_pred) / var_y
+        # y_pred, y_true = b_values.cpu().numpy(), b_returns.cpu().numpy()
+        # var_y = np.var(y_true)
+        # explained_var = np.nan if var_y == 0 else 1 - np.var(y_true - y_pred) / var_y
 
-        # TRY NOT TO MODIFY: record rewards for plotting purposes
-        writer.add_scalar("charts/learning_rate", optimizer.param_groups[0]["lr"], global_step)
-        writer.add_scalar("losses/value_loss", v_loss.item(), global_step)
-        writer.add_scalar("losses/policy_loss", pg_loss.item(), global_step)
-        writer.add_scalar("losses/entropy", entropy_loss.item(), global_step)
-        writer.add_scalar("losses/old_approx_kl", old_approx_kl.item(), global_step)
-        writer.add_scalar("losses/approx_kl", approx_kl.item(), global_step)
-        writer.add_scalar("losses/clipfrac", torch.stack(clipfracs).mean(), global_step)
-        writer.add_scalar("losses/explained_variance", explained_var, global_step)
+        # TODO
+        # # TRY NOT TO MODIFY: record rewards for plotting purposes
+        # writer.add_scalar("charts/learning_rate", optimizer.param_groups[0]["lr"], global_step)
+        # writer.add_scalar("losses/value_loss", v_loss.item(), global_step)
+        # writer.add_scalar("losses/policy_loss", pg_loss.item(), global_step)
+        # writer.add_scalar("losses/entropy", entropy_loss.item(), global_step)
+        # writer.add_scalar("losses/old_approx_kl", old_approx_kl.item(), global_step)
+        # writer.add_scalar("losses/approx_kl", approx_kl.item(), global_step)
+        # writer.add_scalar("losses/clipfrac", torch.stack(clipfracs).mean(), global_step)
+        # writer.add_scalar("losses/explained_variance", explained_var, global_step)
         print(f"speed: {global_step / (time.time() - start_time): 4.4f} sps")
-        writer.add_scalar("charts/SPS", int(global_step / (time.time() - start_time)), global_step)
+        # writer.add_scalar("charts/SPS", int(global_step / (time.time() - start_time)), global_step)
 
     envs.close()
     writer.close()
